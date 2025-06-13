59683627719525098dd607883c32fb46217a5852
syqwq-OMG <3422403944@qq.com>
2025-06-13 22:40:25 +0800

    aw

---
+++ acwing/180.cpp
@@ -0,0 +1,159 @@
+#include <bits/stdc++.h>
+#include <queue>
+using namespace std;
+#define cint const int
+#define cdouble const double
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> PII;
+typedef vector<ll> vi;
+typedef vector<PII> vpii;
+template <class T>
+using vc = vector<T>;
+template <class T>
+using vvc = vc<vc<T>>;
+template <class T>
+constexpr T inf = 0;
+template <>
+constexpr int inf<int> = 1'010'000'000;
+template <>
+constexpr ll inf<ll> = 2'020'000'000'000'000'000;
+template <>
+constexpr double inf<double> = inf<ll>;
+#define fi first
+#define se second
+// https://trap.jp/post/1224/
+#define rep1(a) for (ll _ = 0; _ < ll(a); _++)
+#define rep2(i, a) for (ll i = 1; i <= ll(a); i++)
+#define rep3(i, a, b) for (ll i = (a); i <= ll(b); i++)
+#define rep4(i, a, b, d) for (ll i = (a); i <= ll(b); i += (d))
+#define rp3(i, a, b) for (ll i = (a); i < ll(b); i++)
+#define rp4(i, a, b, d) for (ll i = (a); i < ll(b); i += (d))
+#define per2(i, a) for (ll i = (a); i >= 1; i--)
+#define per3(i, a, b) for (ll i = (a); i >= ll(b); i--)
+#define per4(i, a, b, d) for (ll i = (a); i >= ll(b); i -= (d))
+#define pr3(i, a, b) for (ll i = (a) - 1; i >= ll(b); i--)
+#define pr4(i, a, b, d) for (ll i = (a) - 1; i >= ll(b); i -= (d))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rp(...) overload4(__VA_ARGS__, rp4, rp3)(__VA_ARGS__)
+#define per(...) overload4(__VA_ARGS__, per4, per3, per2)(__VA_ARGS__)
+#define pr(...) overload4(__VA_ARGS__, pr4, pr3)(__VA_ARGS__)
+#define mset(f, z) memset(f, z, sizeof(f))
+#define elif else if
+#define all(x, n) x + 1, x + 1 + n
+#define MIN(v, n) *min_element(all(v, n))
+#define MAX(v, n) *max_element(all(v, n))
+#define LB(c, n, x) distance(c, lower_bound(all(c, n), (x)))
+#define UB(c, n, x) distance(c, upper_bound(all(c, n), (x)))
+auto chmax = [](auto &_a, const auto &_b) -> bool { return _a < _b ? _a = _b, 1 : 0; };
+auto chmin = [](auto &_a, const auto &_b) -> bool { return _a > _b ? _a = _b, 1 : 0; };
+template <class T>
+void wt(const T _x) { cout << _x; }
+template <>
+void wt(const PII _x) { cout << _x.fi << " " << _x.se; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail) {
+    wt(head);
+    if (sizeof...(Tail)) wt(' ');
+    print(std::forward<Tail>(tail)...);
+}
+template <class T>
+void aprint(const T *_arr, int _l, int _r) {
+    if (_l <= _r) rep(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+    else per(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+}
+ll gcd(ll _x, ll _y) { return _y ? gcd(_y, _x % _y) : _x; }
+ll qmi(ll _x, ll _y, ll _mod) {
+    ll _res = 1;
+    for (ll _t = _x; _y; _y >>= 1, _t = _t * _t % _mod)
+        if (_y & 1) _res = _res * _t % _mod;
+    return _res;
+}
+void YES(bool t = 1) { cout << (t ? "YES" : "NO") << endl; }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { cout << (t ? "Yes" : "No") << endl; }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { cout << (t ? "yes" : "no") << endl; }
+void no(bool t = 1) { yes(!t); }
+cint PRECISION = 5;
+// #define int long long
+// #define CF
+// ===========================================================
+// Problem: 第K短路
+// URL: https://www.acwing.com/problem/content/180/
+// ===========================================================
+cint N = 1005;
+
+struct node {
+    ll f, dis, ver;
+    bool operator<(const node &p) const { return f > p.f; }
+};
+
+int n, m;
+vpii g[N], gt[N];
+int dis[N];
+
+void add(vpii *g, int u, int v, int w) { g[u].push_back({v, w}); }
+
+void dijkstra(vpii *g, int s) {
+    rep(i, n) dis[i] = inf<int>;
+    priority_queue<PII> q;
+    bitset<N> st;
+    dis[s] = 0, q.push({0, s});
+    while (q.size()) {
+        int u = q.top().se;
+        q.pop();
+        if (st[u]) continue;
+        st[u] = 1;
+        for (auto [v, w] : g[u])
+            if (chmin(dis[v], dis[u] + w)) q.push({-dis[v], v});
+    }
+}
+
+int bfs(int s, int e, int k) {
+    if (dis[s] == inf<int>) return -1;
+    auto f = [](int x, int d) { return dis[x] + d; };
+
+    int cnt[N];
+    priority_queue<node> q;
+    q.push({f(s, 0), 0, s});
+    while (q.size()) {
+        auto [_, d, u] = q.top();
+        q.pop();
+        cnt[u]++;
+        if (cnt[e] == k) return d;
+        for (auto [v, w] : g[u])
+            if (cnt[v] < k) q.push({f(v, d + w), d + w, v});
+    }
+    return -1;
+}
+
+void solve() {
+    cin >> n >> m;
+    rep(m) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        add(g, u, v, w), add(gt, v, u, w);
+    }
+    int s, e, k;
+    cin >> s >> e >> k;
+    if (s == e) k++;
+    dijkstra(gt, e);
+    print(bfs(s, e, k));
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.setf(ios::fixed), cout.precision(PRECISION);
+    // ================================================
+    int T = 1;
+#ifdef CF
+    cin >> T;
+#endif
+    rep(T) solve();
+    // ================================================
+    return 0;
+}
---
+++ acwing/181.cpp
@@ -0,0 +1,169 @@
+#include <bits/stdc++.h>
+#include <functional>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+#define cint const int
+#define cdouble const double
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> PII;
+typedef vector<ll> vi;
+typedef vector<PII> vpii;
+template <class T>
+using vc = vector<T>;
+template <class T>
+using vvc = vc<vc<T>>;
+template <class T>
+constexpr T inf = 0;
+template <>
+constexpr int inf<int> = 1'010'000'000;
+template <>
+constexpr ll inf<ll> = 2'020'000'000'000'000'000;
+template <>
+constexpr double inf<double> = inf<ll>;
+#define fi first
+#define se second
+// https://trap.jp/post/1224/
+#define rep1(a) for (ll _ = 0; _ < ll(a); _++)
+#define rep2(i, a) for (ll i = 1; i <= ll(a); i++)
+#define rep3(i, a, b) for (ll i = (a); i <= ll(b); i++)
+#define rep4(i, a, b, d) for (ll i = (a); i <= ll(b); i += (d))
+#define rp3(i, a, b) for (ll i = (a); i < ll(b); i++)
+#define rp4(i, a, b, d) for (ll i = (a); i < ll(b); i += (d))
+#define per2(i, a) for (ll i = (a); i >= 1; i--)
+#define per3(i, a, b) for (ll i = (a); i >= ll(b); i--)
+#define per4(i, a, b, d) for (ll i = (a); i >= ll(b); i -= (d))
+#define pr3(i, a, b) for (ll i = (a) - 1; i >= ll(b); i--)
+#define pr4(i, a, b, d) for (ll i = (a) - 1; i >= ll(b); i -= (d))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rp(...) overload4(__VA_ARGS__, rp4, rp3)(__VA_ARGS__)
+#define per(...) overload4(__VA_ARGS__, per4, per3, per2)(__VA_ARGS__)
+#define pr(...) overload4(__VA_ARGS__, pr4, pr3)(__VA_ARGS__)
+#define mset(f, z) memset(f, z, sizeof(f))
+#define elif else if
+#define all(x, n) x + 1, x + 1 + n
+#define MIN(v, n) *min_element(all(v, n))
+#define MAX(v, n) *max_element(all(v, n))
+#define LB(c, n, x) distance(c, lower_bound(all(c, n), (x)))
+#define UB(c, n, x) distance(c, upper_bound(all(c, n), (x)))
+auto chmax = [](auto &_a, const auto &_b) -> bool { return _a < _b ? _a = _b, 1 : 0; };
+auto chmin = [](auto &_a, const auto &_b) -> bool { return _a > _b ? _a = _b, 1 : 0; };
+template <class T>
+void wt(const T _x) { cout << _x; }
+template <>
+void wt(const PII _x) { cout << _x.fi << " " << _x.se; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail) {
+    wt(head);
+    if (sizeof...(Tail)) wt(' ');
+    print(std::forward<Tail>(tail)...);
+}
+template <class T>
+void aprint(const T *_arr, int _l, int _r) {
+    if (_l <= _r) rep(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+    else per(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+}
+ll gcd(ll _x, ll _y) { return _y ? gcd(_y, _x % _y) : _x; }
+ll qmi(ll _x, ll _y, ll _mod) {
+    ll _res = 1;
+    for (ll _t = _x; _y; _y >>= 1, _t = _t * _t % _mod)
+        if (_y & 1) _res = _res * _t % _mod;
+    return _res;
+}
+void YES(bool t = 1) { cout << (t ? "YES" : "NO") << endl; }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { cout << (t ? "Yes" : "No") << endl; }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { cout << (t ? "yes" : "no") << endl; }
+void no(bool t = 1) { yes(!t); }
+cint PRECISION = 5;
+// #define int long long
+// #define CF
+// ===========================================================
+// Problem: 八数码
+// URL: https://www.acwing.com/problem/content/181/
+// ===========================================================
+using PIS = pair<int, string>;
+cint dx[] = {0, 0, -1, 1};
+cint dy[] = {-1, 1, 0, 0};
+const string tar = "12345678x";
+const string cd = "lrud";
+
+string start;
+
+void bfs(string s) {
+    unordered_map<string, int> dis;
+    unordered_map<string, pair<char, string>> p;
+    priority_queue<PIS, vc<PIS>, greater<PIS>> q;
+
+    auto check = [](int x, int y) { return x >= 0 && x < 3 && y >= 0 && y < 3; };
+
+    auto f = [](string s) {
+        int cnt = 0;
+        rp(i, 0, s.size()) {
+            if (s[i] == 'x') continue;
+            int t = s[i] - '1';
+            cnt += abs(i / 3 - t / 3) + abs(i % 3 - t % 3);
+        }
+        return cnt;
+    };
+
+    auto dfs = [&](auto &&self, string s) {
+        if (s == start) return;
+        self(self, p[s].se);
+        wt(p[s].fi);
+    };
+
+    dis[s] = 0,
+    q.push({f(s), s});
+    while (q.size()) {
+        string t = q.top().se;
+        q.pop();
+        if (t == tar) return dfs(dfs, t);
+
+        int x, y;
+        rp(i, 0, t.size()) if (t[i] == 'x') {
+            x = i / 3, y = i % 3;
+            break;
+        }
+
+        rep(i, 0, 3) {
+            int xx = x + dx[i], yy = y + dy[i];
+            if (!check(xx, yy)) continue;
+            string tt = t;
+            swap(tt[xx * 3 + yy], tt[x * 3 + y]);
+            if (dis.count(tt) == 0 || dis[tt] > dis[t] + 1)
+                dis[tt] = dis[t] + 1, p[tt] = {cd[i], t}, q.push({dis[tt] + f(tt), tt});
+        }
+    }
+}
+
+void solve() {
+    string t;
+    char c;
+    while (cin >> c) {
+        start += c;
+        if (c != 'x') t += c;
+    }
+    int cnt = 0;
+    rep(i, 0, 7) rep(j, i, 7) cnt += t[i] > t[j];
+    if (cnt & 1) print("unsolvable");
+    else bfs(start);
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.setf(ios::fixed), cout.precision(PRECISION);
+    // ================================================
+    int T = 1;
+#ifdef CF
+    cin >> T;
+#endif
+    rep(T) solve();
+    // ================================================
+    return 0;
+}
