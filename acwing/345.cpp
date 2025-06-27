1b4d41a8317ea5a19d11090a3882436ece26eed6
syqwq-OMG <3422403944@qq.com>
2025-06-27 16:59:12 +0800

    unfinish

---
+++ acwing/345.cpp
@@ -0,0 +1,150 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define cint const int
+#define cdouble const double
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<ll, ll> PII;
+typedef vector<ll> vi;
+typedef vector<PII> vpii;
+template <class T>
+using vc = vector<T>;
+template <class T>
+using vvc = vc<vc<T>>;
+template <class T>
+constexpr T inf = 0;
+template <>
+constexpr int inf<int> = 1'010'000'000;
+template <>
+constexpr ll inf<ll> = 2'020'000'000'000'000'000;
+template <>
+constexpr double inf<double> = inf<ll>;
+#define fi first
+#define se second
+// https://trap.jp/post/1224/
+#define rep1(a) for (ll _ = 0; _ < ll(a); _++)
+#define rep2(i, a) for (ll i = 1; i <= ll(a); i++)
+#define rep3(i, a, b) for (ll i = (a); i <= ll(b); i++)
+#define rep4(i, a, b, d) for (ll i = (a); i <= ll(b); i += (d))
+#define rp3(i, a, b) for (ll i = (a); i < ll(b); i++)
+#define rp4(i, a, b, d) for (ll i = (a); i < ll(b); i += (d))
+#define per2(i, a) for (ll i = (a); i >= 1; i--)
+#define per3(i, a, b) for (ll i = (a); i >= ll(b); i--)
+#define per4(i, a, b, d) for (ll i = (a); i >= ll(b); i -= (d))
+#define pr3(i, a, b) for (ll i = (a) - 1; i >= ll(b); i--)
+#define pr4(i, a, b, d) for (ll i = (a) - 1; i >= ll(b); i -= (d))
+#define overload4(a, b, c, d, e, ...) e
+#define rep(...) overload4(__VA_ARGS__, rep4, rep3, rep2, rep1)(__VA_ARGS__)
+#define rp(...) overload4(__VA_ARGS__, rp4, rp3)(__VA_ARGS__)
+#define per(...) overload4(__VA_ARGS__, per4, per3, per2)(__VA_ARGS__)
+#define pr(...) overload4(__VA_ARGS__, pr4, pr3)(__VA_ARGS__)
+#define mset(f, z) memset(f, z, sizeof(f))
+#define elif else if
+#define all(x, n) x + 1, x + 1 + n
+#define MIN(v, n) *min_element(all(v, n))
+#define MAX(v, n) *max_element(all(v, n))
+auto chmax = [](auto &_a, const auto &_b) -> bool { return _a < _b ? _a = _b, 1 : 0; };
+auto chmin = [](auto &_a, const auto &_b) -> bool { return _a > _b ? _a = _b, 1 : 0; };
+template <class T>
+void wt(const T _x) { cout << _x; }
+template <>
+void wt(const PII _x) { cout << _x.fi << " " << _x.se; }
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head &&head, Tail &&...tail) {
+    wt(head);
+    if (sizeof...(Tail)) wt(' ');
+    print(std::forward<Tail>(tail)...);
+}
+template <class T>
+void aprint(const T *_arr, int _l, int _r) {
+    if (_l <= _r) rep(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+    else per(i, _l, _r) cout << _arr[i] << " \n"[i == _r];
+}
+ll gcd(ll _x, ll _y) { return _y ? gcd(_y, _x % _y) : _x; }
+void YES(bool t = 1) { cout << (t ? "YES" : "NO") << endl; }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { cout << (t ? "Yes" : "No") << endl; }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { cout << (t ? "yes" : "no") << endl; }
+void no(bool t = 1) { yes(!t); }
+cint PRECISION = 5;
+// #define int long long
+// #define CF
+// ===========================================================
+// Problem: 排序
+// URL: https://www.acwing.com/problem/content/345/
+// ===========================================================
+cint N = 26;
+
+int n, m;
+bool g[N][N], d[N][N];
+bool st[N];
+
+void floyd() {
+    rp(i, 0, n) rp(j, 0, n) d[i][j] = g[i][j];
+    rp(k, 0, n) rp(i, 0, n) rp(j, 0, n) d[i][j] |= d[i][k] && d[k][j];
+}
+
+int check() {
+    rp(i, 0, n) if (d[i][i]) return 2;
+    rp(i, 0, n) rp(j, 0, n) if (!d[i][j] && !d[j][i]) return 0;
+    return 1;
+}
+
+char get_min() {
+    rp(i, 0, n) {
+        if (st[i]) continue;
+        bool flg = 1;
+        rp(j, 0, n) {
+            if (!st[j] && d[j][i]) {
+                flg = 0;
+                break;
+            }
+        }
+        if (flg) {
+            st[i] = 1;
+            return 'A' + i;
+        }
+    }
+    return '0';
+}
+
+void oho() {
+    mset(g, 0);
+    int type = 0, rd;
+    char c[5];
+    rep(i, m) {
+        cin >> c;
+        if (!type) {
+            g[c[0] - 'A'][c[2] - 'A'] = 1;
+            floyd();
+            type = check();
+            if (type) rd = i;
+        }
+    }
+    if (!type) return print("Sorted sequence cannot be determined.");
+    if (type == 2) return print("Inconsistency found after", rd, "relations.");
+    mset(st, 0);
+    cout << "Sorted sequence determined after " << rd << " relations: ";
+    rep(n) wt(get_min());
+    print(".");
+}
+
+void solve() {
+    while (cin >> n >> m, n || m) oho();
+}
+
+signed main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.setf(ios::fixed), cout.precision(PRECISION);
+    // ================================================
+    int T = 1;
+#ifdef CF
+    cin >> T;
+#endif
+    rep(T) solve();
+    // ================================================
+    return 0;
+}
